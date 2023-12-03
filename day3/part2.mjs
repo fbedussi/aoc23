5fa31a9f50eb6dbcb7d8d8c2bf8bbadab0b0fcaf
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-12-03 10:44:03 +0100

    day3 - part2

--- day3/part2.mjs
+++ day3/part2.mjs
@@ -6,30 +6,40 @@ const inputTxt = fs.readFileSync(test ? './input-test.txt' : './input.txt', 'utf
 
 const lines = inputTxt.split(/\r?\n/)
 
-const result = lines
-  .map(line => line.split(': ')[1])
-  .map(game => game.split('; ')
-    .map(round => round.split(', ')
-      .reduce((result, line) => {
-        const [number, color] = line.split(' ')
-        result[color] = Number(number)
-        return result
-      }, {red: 0, blue: 0, green: 0})
-    )
-    .reduce((result, round) => {
-      if (round.blue > result.blue) {
-        result.blue = round.blue
-      }
-      if (round.red > result.red) {
-        result.red = round.red
-      }
-      if (round.green > result.green) {
-        result.green = round.green
-      }
-      return result
-    }, {red: 0, blue: 0, green: 0})
-  )
-  .map(game => Object.values(game).reduce((tot, x) => tot * x))
-  .reduce((sum, x) => sum + x)
+let number = []
+const numbersMap = []
+lines.forEach((line, indexY) => {
+  line.split('').concat('.').forEach((char, indexX) => {
+    if (char.match(/[0-9]/)) {
+      number.push(char)
+    } else { 
+      if (number.length) {
+        numbersMap.push({n: Number(number.join('')), y: indexY, startX: indexX - number.length, endX: indexX - 1})
+      }     
+      number = []
+    }
+  })
+})
 
-console.log('result', result)
+const getAdjacentNumbers = (indexY, indexX) => {
+  return numbersMap.filter(n => indexX >= n.startX -1 && indexX <= n.endX + 1 && indexY >= n.y - 1 && indexY <= n.y + 1).map(n => n.n)
+}
+
+const gears = []
+lines.forEach((line, indexY) => {
+  line.split('').forEach((char, indexX) => {
+    if (char === '*') {
+      const adjacentNumbers = getAdjacentNumbers(indexY, indexX)
+      if (adjacentNumbers.length === 2) {
+        gears.push(adjacentNumbers)
+      }
+    }
+  })
+})
+
+const result =  gears
+  .map(([n1, n2]) => n1 * n2)
+  .reduce((sum, n) => sum + n, 0)
+
+console.log(result)
+
