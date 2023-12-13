4d78fed2369a0c344e3c2af2b469eca0550daf04
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-12-13 20:29:55 +0100

    refactor day 13 - part 2

--- day13/part2.mjs
+++ day13/part2.mjs
@@ -1,3 +1,4 @@
+import {assert} from 'console'
 import fs from 'fs'
 
 const test = false
@@ -17,227 +18,11 @@ const maps = lines.reduce((maps, line) => {
   return maps
 }, [[]])
 
-const oldColsLeft_test = [
-  5,
-  undefined,
-]
-
-const oldColsLeft = [
-  undefined,
-  1,
-  11,
-  undefined,
-  1,
-  5,
-  undefined,
-  4,
-  undefined,
-  undefined,
-  3,
-  11,
-  9,
-  1,
-  undefined,
-  undefined,
-  undefined,
-  4,
-  3,
-  undefined,
-  undefined,
-  1,
-  2,
-  undefined,
-  10,
-  4,
-  10,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  1,
-  11,
-  6,
-  3,
-  9,
-  undefined,
-  undefined,
-  undefined,
-  1,
-  undefined,
-  12,
-  2,
-  1,
-  undefined,
-  undefined,
-  8,
-  5,
-  14,
-  undefined,
-  2,
-  1,
-  2,
-  3,
-  3,
-  12,
-  13,
-  undefined,
-  8,
-  6,
-  undefined,
-  undefined,
-  undefined,
-  1,
-  undefined,
-  1,
-  undefined,
-  undefined,
-  11,
-  2,
-  undefined,
-  undefined,
-  undefined,
-  3,
-  undefined,
-  undefined,
-  undefined,
-  5,
-  undefined,
-  undefined,
-  1,
-  undefined,
-  9,
-  undefined,
-  undefined,
-  undefined,
-  10,
-  undefined,
-  undefined,
-  6,
-  8,
-  10,
-  5,
-  10,
-  3,
-  undefined,
-  10,
-  undefined,
-  undefined,
-  4,
-]
-
-const oldLinesAbove_test = [
-  undefined,
-  4,
-]
-
-const oldLinesAbove = [
-  12,
-  undefined,
-  undefined,
-  1,
-  undefined,
-  undefined,
-  7,
-  undefined,
-  5,
-  9,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  16,
-  7,
-  8,
-  undefined,
-  undefined,
-  3,
-  1,
-  undefined,
-  undefined,
-  4,
-  undefined,
-  undefined,
-  undefined,
-  16,
-  8,
-  1,
-  5,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  5,
-  13,
-  5,
-  undefined,
-  3,
-  undefined,
-  undefined,
-  undefined,
-  7,
-  6,
-  undefined,
-  undefined,
-  undefined,
-  8,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  10,
-  undefined,
-  undefined,
-  12,
-  15,
-  1,
-  undefined,
-  3,
-  undefined,
-  5,
-  11,
-  undefined,
-  undefined,
-  1,
-  3,
-  1,
-  undefined,
-  4,
-  5,
-  5,
-  undefined,
-  3,
-  2,
-  undefined,
-  2,
-  undefined,
-  4,
-  2,
-  3,
-  undefined,
-  2,
-  7,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  7,
-  undefined,
-  12,
-  2,
-  undefined,
-]
-
 const flipMap = (map) => map[0].reduce((flippedMap, _, index) => {
-    const flippedLine = map.map(line => line[index]).join('')
-    flippedMap.push(flippedLine)
-    return flippedMap
-  }, [])
+  const flippedLine = map.map(line => line[index]).join('')
+  flippedMap.push(flippedLine)
+  return flippedMap
+}, [])
 
 const flippedMaps = maps
   .map(map => map.map(line => line.split('')))
@@ -260,32 +45,30 @@ const reflection = (map, i) => {
   let delta = 0
   let differences = 0
   while (reflection && delta < i && i + delta < map.length) {
-    differences += diff(map[i - delta - 1],map[i + delta])
+    differences += diff(map[i - delta - 1], map[i + delta])
     reflection = (map[i - delta - 1] === map[i + delta]) || differences === 1
     delta++
   }
-  return reflection
+  return reflection && differences === 1
 }
 
-const numberOfLinesAbove = (map, mapIndex, oldResults) => {
+const numberOfLinesAbove = (map) => {
   let i = 1
   while (i < map.length) {
-    const oldResultIndex = oldResults[mapIndex]
-    if (i !== oldResultIndex) {
-      if (reflection(map, i)) {
-        return i
-      }
+    if (reflection(map, i)) {
+      return i
     }
 
     i++
   }
 }
 
-const colsLeft = flippedMaps.map((map, index) => numberOfLinesAbove(map, index, test ? oldColsLeft_test : oldColsLeft))
+const colsLeft = flippedMaps.map(numberOfLinesAbove)
 
-const linesAbove = maps.map((map, index) => numberOfLinesAbove(map, index, test ? oldLinesAbove_test : oldLinesAbove))
+const linesAbove = maps.map(numberOfLinesAbove)
 
 const result = colsLeft.filter(Boolean).reduce((sum, n) => sum + n, 0) + (100 * linesAbove.filter(Boolean).reduce((sum, n) => sum + n, 0))
 
 console.log(result)
 
+assert(result === 31947)
